4cb17ce3c7aebc25d171fec3eaa31f280c824a74
Emily Lau <emilylau@Brucebs-MacBook-Pro-2.local>
2019-07-15 15:32:52 -0600

    JPA and Boot Stub

---
+++ COFish/src/main/java/com/skilldistillery/cofish/CoFishApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.cofish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoFishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoFishApplication.class, args);
+	}
+
+}
---
+++ JPACOFish/src/main/java/com/skilldistillery/cofish/entities/User.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.cofish.entities;
+
+public class User {
+
+}
