915415bf4f8910212cc3a55201c2e2e099b8f859
hatle$ <kayla.hatle@yahoo.com>
2019-07-16 16:18:36 -0600

    finished fishtype entity

--- JPACOFish/src/main/java/com/skilldistillery/cofish/entities/FishType.java
+++ JPACOFish/src/main/java/com/skilldistillery/cofish/entities/FishType.java
@@ -103,5 +103,55 @@ public class FishType {
 	public void setCaughtFishes(List<CaughtFish> caughtFishes) {
 		this.caughtFishes = caughtFishes;
 	}
+
+	//HASHCODE AND EQUALS
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FishType other = (FishType) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	//CONSTRUCTORS
+	
+	public FishType() {
+		super();
+	}
+
+	public FishType(int id, String name, String imageUrl, String description, String wikiUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.imageUrl = imageUrl;
+		this.description = description;
+		this.wikiUrl = wikiUrl;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("FishType [id=").append(id).append(", name=").append(name).append(", imageUrl=").append(imageUrl)
+				.append(", description=").append(description).append(", wikiUrl=").append(wikiUrl).append("]");
+		return builder.toString();
+	}
+	
+	
+	
+	
 	
 }
